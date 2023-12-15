f98aee1d0162d6cbefd28b4e45b124cc8cd55c89
habeeb-an <hbareacode@gmail.com>
2023-12-15 08:08:15 +0530

    signup form created& implemented toggle function betweeen signup and login

---
+++ script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+   
+    document.getElementById('show-signup').addEventListener('click',function(){
+        document.getElementById('login-form').classList.add('hidden')
+        document.getElementById('signup-form').classList.remove('hidden')
+    })
+    document.getElementById('show-login').addEventListener('click',function(){
+        document.getElementById('signup-form').classList.add('hidden')
+        document.getElementById('login-form').classList.remove('hidden')
+    })
+
+    
+
+})
+
