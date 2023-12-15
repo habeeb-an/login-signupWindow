c0b89986fa1f1a6ceae8c6d9495b43803cd6a8a4
habeeb-an <hbareacode@gmail.com>
2023-12-15 08:57:33 +0530

    more validation added for user sigup/login fields

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener('DOMContentLoaded',()=>{
 //users data
     const users = [
-        { username: 'user1', email: 'user1@example.com', password: 'password1' },
+        { username: 'user1', email: 'user1@gmail.com', password: 'password1' },
 
     ];
+
+    
    // signup/login toggle
     document.getElementById('show-signup').addEventListener('click',()=>{
         document.getElementById('login-form').classList.add('hidden')
@@ -13,12 +15,28 @@ document.addEventListener('DOMContentLoaded',()=>{
         document.getElementById('signup-form').classList.add('hidden')
         document.getElementById('login-form').classList.remove('hidden')
     })
+    //emailformat validation
+    function emailValidation(email){
+        const regex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        console.log(regex.test(email))
+        return regex.test(email);
+    }
     // Login functionality
     document.getElementById('login-button').addEventListener('click',(ev)=>{
         ev.preventDefault();
         const email=document.getElementById('login-email').value;
         const password=document.getElementById('login-password').value;
-        const user=users.find(i=>i.email===email&&i.password===password)
+        //email and password validation
+        if(!email || !emailValidation(email)){
+            alert('Enter a valid email address')
+            return;
+        }
+        if (!password) {
+            alert('Enter your password.');
+            return;
+        }
+     
+    const user=users.find(i=>i.email===email&&i.password===password)
 
         if(user){
             alert('Login successful')
@@ -33,12 +51,28 @@ document.addEventListener('DOMContentLoaded',()=>{
         const username=document.getElementById('signup-username').value;
         const email=document.getElementById('signup-email').value;
         const password=document.getElementById('signup-password').value;
-        const passwordMatch=document.getElementById('signup-password').value;
+        const passwordMatch=document.getElementById('match-password').value;
+
+     // validations
+    if (!username) {
+            alert('Enter a username.');
+            return;
+        }
+     console.log({password,passwordMatch});
    
     if(password != passwordMatch){
         alert('Passwords do not match.');
         return;
     }
+     if(!email || !emailValidation(email)){
+        alert('Enter a valid email address')
+        return;
+    }
+    if (!password) {
+        alert('Eter your password.');
+        return;
+    }
+ 
     //email validatior
     if(users.some(user=>user.email===email)){
         alert('Email already exists')
